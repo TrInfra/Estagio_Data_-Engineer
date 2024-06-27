2b041cc404564b40f228d1a0bf5a0bdc71ab0c2f
Nyck Bastos <90692553+TrInfra@users.noreply.github.com>
2024-06-27 18:15:57 -0300

    Add: Adicionando repositório de exercicios

--- Sprint 6/Desafio/Aprendizados/Aprendizados.md
+++
@@ -1,9 +0,0 @@
-## Aprendizados
-
-Nessa sprint pratiquei as principais ferramentas da aws:
- - Amazon EC2 (Elastic Compute Cloud)
- - Amazon S3 (Simple Storage Service)
- - Amazon EBS (Elastic Block Store)
- - Amazon VPC (Virtual Private Cloud)
- - AWS IAM (Identity and Access Management)
- - Amazon EFS (Elastic File System) 
---
+++ Sprint 6/exercicios/Ex02/Ex02.sql
@@ -0,0 +1,35 @@
+-- Criando a tabela e as colunas
+CREATE EXTERNAL TABLE IF NOT EXISTS meubanco.person (
+  nome varchar(180),
+  sexo char(1),
+  total integer,
+  ano integer
+)
+ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.lazy.LazySimpleSerDe'
+WITH SERDEPROPERTIES (
+ 'serialization.format' = ',',
+ 'field.delim' = ','
+)
+LOCATION 's3://exercicio01sprint6.com/dados/';
+
+SELECT 
+    sub.decada,
+    sub.nome,
+    sub.total_decada
+FROM (
+    SELECT 
+        nome,
+        (ano / 10) * 10 AS decada,
+        SUM(total) AS total_decada,
+        ROW_NUMBER() OVER (PARTITION BY (ano / 10) * 10 ORDER BY SUM(total) DESC) AS posicao
+    FROM 
+        person
+    WHERE 
+        ano >= 1950
+    GROUP BY 
+        nome, (ano / 10) * 10
+) AS sub
+WHERE 
+    sub.posicao <= 3
+ORDER BY 
+    sub.decada, sub.posicao;
